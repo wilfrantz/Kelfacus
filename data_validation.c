48ceff0f49ebd9743f12c8f9ea29c23c30a13ccc
Wilfrantz <wilfrantz@outlook.com>
2019-10-07 23:56:37 +0100

    Cleaning up

--- data_validation.c
+++ data_validation.c
@@ -1,38 +1,18 @@
 //
 //  data_validation.c
 //  Tempature converter
-//
 //  Created by Wilfrantz DEDE on 3/7/19.
 //  Copyright © 2019 Wilfrantz DEDE. All rights reserved.
-//
 
 #include <stdio.h>
-
+#include <stdbool.h>
 double data_validation(double celcius)
-
-{
-
-// Variable declaration
-    
-    char c;
-    
-// Data validation
-    
-  
-   
-    while ( !isdigit (celcius) == 0)
-        
+    {
+    // Data validation
+    while (isdigit (celcius) == false)
     {
         printf ("\nInvalid entry. Please try again: ");
         scanf("%lf", &celcius);
-        while ( (c = getchar() != '\n') && c != EOF); // clear input buffer.
-        
     }
-    
-    
-    
-
-    
- return celcius;
-  
+    return celcius;
 }

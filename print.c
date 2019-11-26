d73a0475a4765bea0c5c440fd03219a1ff8bf81e
Wilfrantz <wilfrantz@outlook.com>
2019-11-26 21:23:44 +0100

    minor fixes

--- print.c
+++ print.c
@@ -5,7 +5,7 @@
 int print (var Celcius, var Fahrenheit, var Kelvin)
 {
     // write to stdout
-    printf("\n\n");
+    printf("\n");
     printf("\n\tCelcius: %.2f \n\tFahrenheit: %.2f \n\tKelvin: %.f\n", Celcius.num, Fahrenheit.num, Kelvin.num);
 
     return 0;

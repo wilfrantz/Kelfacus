f3bee5b9e675d9c2d15b166059b415a4f165fc5e
Wilfrantz <wilfrantz@outlook.com>
2020-04-10 19:42:26 +0100

    shrinking

--- cpp/print.cpp
+++ cpp/print.cpp
@@ -5,9 +5,6 @@
 
 int print (Var* obj_Ptr){
 
-    // Class object.
-    //Var obj;
-
     // write to stdout
     std::cout << std::endl;
     std::cout<<"\n\tCelcius: "<<obj_Ptr->getCelcius() << " \n\tFahrenheit: "<<obj_Ptr->getFahrenheit() <<" \n\tKelvin: "<<obj_Ptr->getKelvin()<< std::endl;

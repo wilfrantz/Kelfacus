1b2acec0f9c0daba052482e09ce6d0742bd8a3d4
Wilfrantz <wilfrantz@outlook.com>
2020-04-10 21:23:34 +0100

    shrinking and optimization

--- cpp/data.h
+++ cpp/data.h
@@ -5,36 +5,22 @@
 #ifndef _DATAH_
 #define _DATAH_
 
+#include <iostream>
+
 class Var {
 
 	public:	
 
 
 		// Setter functions
-		void setCelcius(float cel) {
-			celcius = cel ;
-		}
-
-		void setFahrenheit (float fa){
-			fahrenheit = fa;
-		}
-
-		void setKelvin (float kel){
-			kelvin = kel;
-		}
+		void setCelcius(float cel) {celcius = cel ; }
+		void setFahrenheit (float fa){fahrenheit = fa;}
+		void setKelvin (float kel){kelvin = kel; }
 
 		// Getter functions
-		float getCelcius(){
-			return celcius;
-		}
-
-		float getFahrenheit(){
-			return fahrenheit;
-		}
-
-		float getKelvin(){
-			return kelvin;
-		}
+		float getCelcius(){return celcius; }
+		float getFahrenheit(){return fahrenheit; }
+		float getKelvin(){return kelvin; }
 
 	private:
 		float celcius, fahrenheit, kelvin;
@@ -43,9 +29,9 @@ class Var {
 #endif // !DATAH
 
 // Functions declaration.
-int Convert_from_Fahrenheit(char*);
-int Convert_from_Kelvin(char*);
+int Convert_from_Fahrenheit(std::string);
+int Convert_from_Kelvin(std::string);
 int print (Var*);
-int Convert_from_Celcius(char*);
+int Convert_from_Celcius(std::string);
 // Var data_validation();
 

4fc22adff06381239e75c3ec1143ab15633ccd43
Wilfrantz <wilfrantz@outlook.com>
2020-04-10 19:41:25 +0100

    remove unnecessary header

--- cpp/print.cpp
+++ cpp/print.cpp
@@ -10,7 +10,6 @@ int print (Var* obj_Ptr){
 
     // write to stdout
     std::cout << std::endl;
-
     std::cout<<"\n\tCelcius: "<<obj_Ptr->getCelcius() << " \n\tFahrenheit: "<<obj_Ptr->getFahrenheit() <<" \n\tKelvin: "<<obj_Ptr->getKelvin()<< std::endl;
 
     return 0;
